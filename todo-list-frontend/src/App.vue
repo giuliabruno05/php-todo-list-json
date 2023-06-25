d6f29fe1e90017a794bba80325c9385c19c1d934
giuliabruno05 <giuliabruno48@gmail.com>
2023-06-25 23:00:25 +0200

    aggiunto toggle alle task e completato il layout tramite scss

--- todo-list-frontend/src/App.vue
+++ todo-list-frontend/src/App.vue
@@ -38,7 +38,15 @@ export default {
                     const data = res.data;
                     this.todos = data;
                 });
-        }
+        },
+        toggleLine(indice) {
+            // this.todos[indice].status = !this.todos[indice].status
+            if (this.todos[indice].status === 'true') {
+                this.todos[indice].status = 'false'
+            } else {
+                this.todos[indice].status = 'true'
+            }
+        },
     },
     mounted() {
 
@@ -58,18 +66,24 @@ export default {
     <div class="container">
         <ul>
             <li v-for="(todo, i) in todos " :key="i">
-                {{ todo.todo }}
-                <button @click="deleteTodo(i)"><i class="fa-regular fa-trash-can"></i></button>
+                <span :class="todo.status === 'true' ? 'line-through' : ''" @click="toggleLine(i)">{{ todo.todo }}</span>
+                <button @click="deleteTodo(i)"><i class="fa-solid fa-trash"></i></button>
             </li>
         </ul>
-        <form @submit.prevent="onSubmit">
-            <input type="text" name="todo" v-model="newtodos.todo">
-            <input type="submit" value="inserisci">
-        </form>
     </div>
+    <form @submit.prevent="onSubmit">
+        <input type="text" name="todo" v-model="newtodos.todo" placeholder="Inserisci un elemento...">
+        <input class="submit" type="submit" value="Inserisci">
+    </form>
 </template>
 
 <style lang="scss">
+* {
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Inter', sans-serif;
+}
+
 body {
     background-color: #001632;
     text-align: center;
@@ -80,20 +94,60 @@ body {
 
     .container {
         background-color: white;
-        width: 30%;
+        width: 35%;
         margin: 0 auto;
-        padding: 20px;
+        border-radius: 8px;
 
         ul {
             list-style: none;
 
+
             li {
                 display: flex;
-                justify-content: space-between
+                justify-content: space-between;
+                padding: 10px 0;
+                border-bottom: 1px solid lightgray;
+                padding: 10px 20px;
+                align-items: center;
+
+                &:last-child {
+                    border: hidden;
+                }
+
+            }
+
+            .line-through {
+                text-decoration: line-through;
+                cursor: pointer;
+            }
+
+            button {
+                background-color: #db3243;
+                padding: 10px;
+                color: white;
+                border: hidden;
+                border-radius: 5px;
             }
         }
     }
 
+    input {
+        border-radius: 3px 0 0 3px;
+        padding: 8px;
+        margin: 20px 0;
+        border: 0.5px solid lightgray;
+        width: calc(35vw - 65px);
+    }
+
+    .submit {
+        color: #d19a15;
+        background-color: #001632;
+        border: 0.5px solid transparent;
+        border-color: #d19a15;
+        border-radius: 0 3px 3px 0;
+        width: 65px;
+
+    }
 
 }
 </style>
